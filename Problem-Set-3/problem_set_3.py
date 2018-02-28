a5dae42f7c2caeee6cd3cf2a7e8571d6fbab3fab
Reid Gahan <rrgahan@gmail.com>
2018-02-28 13:31:08 -0600

    Random Forest is working, but with dropping all NaN's

--- Problem-Set-3/problem_set_3.py
+++ Problem-Set-3/problem_set_3.py
@@ -1,7 +1,8 @@
+import numpy as np
 import pandas as pd
 import sys
 
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 
 def main():
@@ -11,25 +12,50 @@ def main():
 
 def titanic():
     filename = 'data/titanic_train.csv'
-    train = load_data_frame(filename)
+    train = load_data_frame(filename).dropna()
 
     X_train, Y_train = split_data_frame(train, 'Survived')
-    X_train["Embarked"] = clean_column(X_train, ["Embarked"])
-    X_train["Sex"] = clean_column(X_train, ["Sex"])
-    X_train = X_train.drop(["Cabin", "Ticket", "Name"], axis=1)
+    X_train = X_train.drop(["Name", "Sex", "Ticket", "Cabin", "Embarked"], axis=1).astype(np.float32)
 
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(X_train, Y_train)
+    describe_data_frame(X_train)
+
+    clf = RandomForestClassifier()
+    clf.fit(X_train, Y_train)
 
     filename = 'data/titanic_test.csv'
-    X_test = load_data_frame(filename)
-    X_test["Embarked"] = clean_column(X_test, ["Embarked"])
-    X_test["Sex"] = clean_column(X_test, ["Sex"])
-    X_test = X_test.drop(["Cabin", "Ticket", "Name"], axis=1)
+    X_test = load_data_frame(filename).dropna()
 
-    clf.predict(X_test)
+    X_test = X_test.drop(["Name", "Sex", "Ticket", "Cabin", "Embarked"], axis=1).astype(np.float32)
+
+    print(clf.predict(X_test))
 
     return
+
+
+def describe_data_frame(dataframe):
+    """
+    Aimed at describing characteristics data set for the
+    following parts:
+
+    1. How many observations are in the data set?
+    2. How many features are in the data set?
+    3. What is the data type for each feature and the response?
+    4. Print the mean, median, max, and min values for each feature. Suggest using summary()
+    """
+    print(">> Data Frame Description\n")
+
+    rows, columns = dataframe.shape
+    print("num rows (observations): " + str(rows))
+    print("num cols (features): " + str(columns))
+    print("\n")
+
+    print(str(dataframe.info()))
+
+    print("\nData Set Statistics")
+    print("=====================")
+
+    print(str(dataframe.describe()))
+    print("\n")
 
 
 def load_data_frame(filename):
@@ -49,7 +75,7 @@ def split_data_frame(df, label):
     return X, Y
 
 
-def clean_column(df, label):
+def encode_column(df, label):
     binary_encoding = pd.get_dummies(df[label])
     encoded_vector = binary_encoding.values.argmax(1)
     return encoded_vector
