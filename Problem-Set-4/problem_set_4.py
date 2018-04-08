3ec5f107f191990591c624868959c6a0703a4358
Jacob Shiohira <jlshiohira23@gmail.com>
2018-04-08 15:57:00 -0500

    Add code for custom linear regression and computation of the majority of model statistics

--- Problem-Set-4/problem_set_4.py
+++ Problem-Set-4/problem_set_4.py
@@ -2,7 +2,7 @@ import sys
 import pydot
 import numpy as np
 import pandas as pd
-import math as Math
+import math as  math
 import matplotlib.pyplot as plt
 import operator
 
@@ -10,11 +10,12 @@ import pprint
 
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+from scipy import stats
 from sklearn import linear_model
 from sklearn.preprocessing import QuantileTransformer, Normalizer, LabelEncoder
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error, confusion_matrix, classification_report, accuracy_score
+from sklearn.metrics import mean_squared_error, confusion_matrix, classification_report, accuracy_score, r2_score
 from sklearn.dummy import DummyClassifier
 from statsmodels.regression.linear_model import OLS
 from statsmodels.tools import add_constant
@@ -36,9 +37,19 @@ from statsmodels.tools import add_constant
 #   Output variable (based on sensory data): 
 #   12 - quality (score between 0 and 10)
 
+'''
+    [TODO] Question 10
+        - Reduce the number of explanatory variables in your lm() model one by one to find the best model using
+          the AIC criterion (tradeoff between maximum likelihood and number of parameters). (suggest using
+          step(lm(),…))
+        - Increase the number explanatory variables from the intercept alone in your lm() model one by one to
+          find the best model using the AIC criterion
+        - Note that step(lm()) uses extractAIC() not AIC()
+'''
+
 def main():
     '''
-        Solution for Problem Set  in Data & Models II (RAIK 370H)
+        Solution for Problem Set 4 in Data & Models II (RAIK 370H)
 
         All of the functions pertaining to the set of questions are below
         and you should just be able to copy and paste the function name
@@ -46,7 +57,6 @@ def main():
         results. Each function itself describes what it's doing along
         with the parts of the problem set question it addresses.
     '''
-
     plot_flag = True
 
     filename = 'Data/winequality-white.csv'
@@ -58,11 +68,15 @@ def main():
     find_baseline(X_train, X_test, Y_train, Y_test)
 
     scaled_df = scale_dataset(df)
-    X, Y = create_matrix_and_vector_from_data_frame(scaled_df)
-    X_train, X_test, Y_train, Y_test = create_train_test_set(X, Y)
-    create_linear_model(X_train, X_test, Y_train, Y_test)
-    compute_model_parameters(X, Y)
-    gradient_descent_solver(X, Y)
+    scaled_X, scaled_Y = create_matrix_and_vector_from_data_frame(scaled_df)
+    scaled_X_train, scaled_X_test, scaled_Y_train, scaled_Y_test = create_train_test_set(scaled_X, scaled_Y)
+    find_baseline(X_train, X_test, Y_train, Y_test)
+
+    coefficients = create_linear_model(X_train, X_test, Y_train, Y_test)
+    # compute_model_parameters(X, Y)
+    # gradient_descent_solver(scaled_X_train, scaled_X_test, scaled_Y_train, scaled_Y_test, coefficients)
+    actual, predictions, betas, nobs, p = create_custom_linear_model(X_train, X_test, Y_train, Y_test, coefficients)
+    compute_custom_model_statistics(X_test, nobs, p, betas, actual, predictions)
 
 
 def load_data_frame(filename):
@@ -357,24 +371,93 @@ def create_train_test_set(X, Y):
     return X_train, X_test, Y_train, Y_test
 
 
-def find_baseline(X_train, X_test, Y_train, Y_test):    
-    fitModel = DummyClassifier(strategy='stratified').fit(X_train, Y_train)
-    baseline_score = fitModel.score(X_test, Y_test)
-    print("Baseline Score (R^2): {}".format(baseline_score))
-    return
+def find_baseline(X_train, X_test, Y_train, Y_test):
+    ''' TODO '''
+    # def loss_func(Y_test, X_test):
+    #     diff = np.abs(ground_truth - predictions).max()
+    #     return np.log(1 + diff)
+
+    reg = DummyClassifier(strategy='stratified').fit(X_train, Y_train)
+    # score = make_scorer(loss_func, greater_is_better=True)
+    
+    # baseline_score = reg.score(X_test, Y_test)
+    # print("Baseline Score (R^2): {}".format(baseline_score))
 
 
 def create_linear_model(X_train, X_test, Y_train, Y_test):
+    ''' TODO...
+        - Predict the wine quality using the test set and compare the accuracy to the actual quality. Comment.
+        - Print the parameter estimates and their 95% confidence intervals in a single table. (Suggest using
+          confint()), and cbind()
+    '''
     X_train = add_constant(X_train)
+    regressionResult = OLS(Y_train, X_train).fit()
+    print(regressionResult.summary())
+
+    # Print various attributes of the OLS fitted model
+    # print("R Squared: {}".format(regressionResult.rsquared))
+    # print("SSE: {}".format(regressionResult.ess))
+    # print("SSR: {}".format(regressionResult.ssr))
+    # print("Residual MSE: {}".format(regressionResult.mse_resid))
+    # print("Total MSE: {}".format(regressionResult.mse_total))
+    # print("Model MSE: {}".format(regressionResult.mse_model))
+    # print("F-Value: {}".format(regressionResult.mse_model/regressionResult.mse_resid))
+    # print("NOBS: {}".format(regressionResult.nobs))
+    # print("Centered TSS: {}".format(regressionResult.centered_tss))
+    # print("Uncentered TSS: {}".format(regressionResult.uncentered_tss))
+    # print("DF Model: {}".format(regressionResult.df_model))
+    # print("DF Resid: {}".format(regressionResult.df_resid))
+
+    predictions = regressionResult.predict(X_train)
+
+    nobs, p = X_train.shape
+    eaic = extractAIC(nobs, p, Y_train, predictions)
+    print("Extract AIC: {}".format(eaic))
+
+    # create linear regression object to grab coefficients
+    reg = linear_model.LinearRegression().fit(X_train, Y_train)
+
+    return reg.coef_
+
+
+def create_custom_linear_model(X_train, X_test, Y_train, Y_test, coefficients):
+    X_train = add_constant(X_train)
+    n, p = X_train.shape
+    betas = np.zeros(p).reshape(p, 1)
+    error_vector = Y_train - X_train.dot(betas)
+    betas = np.dot(np.dot(np.linalg.inv(X_train.T.dot(X_train)), X_train.T), Y_train).reshape(p, 1)
+
+    print("Libraray linear model coefficients: {}".format(coefficients))
+    print("Custom model parameters: {}".format(betas))
+
+    n_test, p_test = X_test.shape
     X_test = add_constant(X_test)
-    reg = OLS(Y_train, add_constant(X_train)).fit()
-    print(reg.summary())
 
-    Y_pred = reg.predict(add_constant(X_test))
+    predictions = X_train.dot(betas).reshape(n,1)
+    rmse = math.sqrt(mean_squared_error(Y_train, predictions))
+    print("Root Mean Squared Error: {}".format(rmse))
 
-    # IDK how we want to plot this, right now it's prediciton vs real.
-    plt.plot(Y_pred, Y_test, 'bo')
-    # plt.show()
+    return Y_train, predictions, betas, n, p
+
+
+def compute_custom_model_statistics(X, nobs, p, betas, actual, predictions):
+    ''' TODO...
+        - [QUESTION] If the errors were not nearly normal what might be the problem?
+        - [QUESTION] Most residual errors are less than 1, what does that mean ?
+    '''
+    plot = False
+
+    actual = actual.reshape(nobs,1)
+    residuals = np.subtract(actual, predictions)
+    num_lt_1 = sum([1 for r in residuals if math.fabs(r) < 1])
+    print("Percent of residuals less than 1: {}%".format((num_lt_1/len(residuals))*100))
+
+    if plot:
+        plot_feature_histograms(pd.DataFrame(residuals))
+
+    # -------------------------------------------------------
+
+    create_summary_table(X, nobs, p, betas, actual, predictions)
     return
 
 
@@ -383,86 +466,204 @@ def compute_model_parameters(X, Y):
     return
 
 
-def gradient_descent_solver(X, Y):
-    def cost(B):
-        e = np.sum((X.dot(B) - Y[0]) ** 2)
-        return e
-
-    n = 12
-    X = add_constant(X)
-    B = np.ones(n).reshape(n, 1)
-    Y = Y.reshape(4898, 1)
-    print(Y)
-    learning_rate = 0.00001
-    convergence = 10
-    c = cost(B)
-
-    while c > convergence:
-        dldw = -2 * X.T.dot(np.subtract(Y[0], X.dot(B)))
-        B = B - dldw * learning_rate
-        c = cost(B)
-        print(c)
-
-    print("Cost: {}".format(c))
-    print("Betas: {}".format(B))
-
-
-'''
-    TODO'S...
-
-    Question 4
-        - Roll your own code to compute model parameters, as well as the model information from the library solver
-        - First create the matrix and the vector for the training data (Remember to insert a column of 1’s in
-          the matrix)
-        - Compute the parameter values (the coefficent estimates in the lm() object )
-        - Print the parameters from the lm() model and from your normal solver side by side. Comment. (suggest
-          using head())
-        - Print the test set quality from the lm() model and from your normal solver side by side. Comment.
-          (suggest using head())
-        - Print the rmse error between the predicted and actual test qualities
-
-    Question 5
-        - Now compute the parameters using the gradient descent solver using the same and
-        - First, write a function to compute the scalar value of the cost function
-        - Clearly display your learning rate, and your convergence criterion
+def gradient_descent_solver(X_train, X_test, Y_train, Y_test, coefficients):
+    ''' TODO...
         - Print the estimated parameters from the lm() model, your normal solver, and your gradient descent
           solver side by side. Comment.
         - Predict the wine quality using the gradient descent parameter using the test set and compare to the
           actual quality in the test set
+    '''
+    def cost(X, Y, B):
+        e = np.sum((X.dot(B) - Y[0]) ** 2)
+        return e
 
-    Question 6
-        - Compare accuracies on the test set to those of a neural net model. Comment.
-        - Describe your final neural net model.
+    learning_rate = 0.00001
+    convergence = 1
+    print("Learning Rate: {}".format(learning_rate))
+    print("Convergence Criterion: {}".format(convergence))
 
-    Question 7
-        - Now re-compute all of the information from your lm() model using your normal equation model
-            - Compute error residuals, e, and plot the histogram of residuals
-            - Print the summary() of the error vector, , and compare to lm() model output. Comment
-            - Plot histogram of residual errors to check approximate normality. If the errors were not nearly normal
-              what might be the problem?
-            - Most residual errors are less than |1|, what does that mean ?
-            - Compute the residual standard error and the degrees freedom for the residual error
-    
-    Question 8
-        - Create and print a table similar to that in lm() output for your theta values for estimates, , compute
-          standard error, T values, and P values.
-    
-    Question 9
-        - Compute R^2
-        - Compute R^2_{ADJ}
-        - Compute AIC
-        - Compute the F statistic for the model
-        - Compute degrees of freedom 1 and 2 for the f diistribution
-        - Compute the P value for the F, overall model, statistic
+    X_train = add_constant(X_train)
+    xtrain_rows, xtrain_columns = X_train.shape
+    n = xtrain_columns
 
-    Question 10
-        - Reduce the number of explanatory variables in your lm() model one by one to find the best model using
-          the AIC criterion (tradeoff between maximum likelihood and number of parameters). (suggest using
-          step(lm(),…))
-        - Increase the number explanatory variables from the intercept alone in your lm() model one by one to
-          find the best model using the AIC criterion
-        - Note that step(lm()) uses extractAIC() not AIC()
-'''
+    B = np.ones(n).reshape(n, 1)
+    Y_train = Y_train.reshape(xtrain_rows, 1)
+    c = cost(X_train, Y_train, B)
+
+    while c > convergence:
+        dldw = -2 * X_train.T.dot(np.subtract(Y_train[0], X_train.dot(B)))
+        B = B - dldw * learning_rate
+        c = cost(X_train, Y_train, B)
+        print(c)
+
+    print("Gradient Descent Final Cost: {}".format(c))
+    print("Gradient Descent Parameters: {}".format(B))
+    print("Libraray linear model coefficients: {}".format(coefficients))
+
+    # Predict the wine quality based on the GD parameters
+    predictions = add_constant(X_test).dot(B)
+    # for i in range(len(predictions)):
+    #     print("Actual quality: {}, Predicted quality: {}".format(Y_test[i], predictions[i]))
+    rmse = math.sqrt(mean_squared_error(Y_test, predictions))
+
+    print("Gradient Descent Root Mean Squared Error: {}".format(rmse))
+
+    for i in range(len(predictions)):
+        print("Actual quality: {}, Predicted quality: {}".format(Y_test[i], predictions[i]))
+
+
+def create_summary_table(X, nobs, p, betas, actual, predictions):
+    '''
+        - Create and print a table similar to that in lm() output for your theta values
+          for estimates, compute standard error, T values, and P values.
+
+          http://www.statsmodels.org/dev/_modules/statsmodels/regression/linear_model.html#RegressionResults.ess
+    '''
+    rows, _ = actual.shape
+    x_n, _ = X.shape
+
+    residuals = np.subtract(actual.reshape(rows, 1), predictions)
+
+    print("\n\nRegression Results Summary Table")
+    print("Residuals:")
+    print("Min\t1Q\tMedian\t3Q\tMax")
+    minimum = round(residuals.min(), 3)
+    quart_one = round(np.percentile(residuals, 25), 3)
+    median = round(np.median(residuals), 3)
+    quart_three = round(np.percentile(residuals, 75), 3)
+    maximum = round(residuals.max(), 3)
+
+    print("{}\t{}\t{}\t{}\t{}".format(minimum, quart_one, median, quart_three, maximum))
+
+    # ------------------ BEGIN TABLE OUTPUT ------------------
+
+    print("\nCoefficients:")
+    print("\t\tEstimate\tStd. Error\tt-value\t\tPr")
+
+    print("(Intercepts)\t{}".format(round(betas[0][0], 3)))
+    for i in range(1, p):
+        print("x{}\t\t{}".format(i, round(betas[i][0], 3)))
+
+    # ------------------ BEGIN FOOTER OUTPUT ------------------
+    sse = calculate_sse(actual, predictions)
+    ssr = calculate_ssr(actual, predictions)
+    sst = calculate_sst(actual, predictions)
+
+    # print("\nSSE: {}".format(sse))
+    # print("SSR: {}".format(ssr))
+    # print("SST: {}".format(sst))
+
+    dfr = nobs-p
+    dfm = p-1
+
+    aic = AIC(nobs, sse, ssr, dfm, actual, predictions)
+    print("Akaike's information criteria (AIC): {}".format(aic))
+
+    rse = sse / dfr
+    print("\nResidual Standard Error {} on {} degrees of freedom.".format(rse, dfr))
+
+    r_squared = round(calculate_r_squared(ssr, sst), 3)
+    adj_r_squared = round(calculate_adj_r_squared(nobs, ssr, sst, dfr, dfm), 3)
+    print("Multiple R-squared: {}, Adjusted R-squared: {}".format(r_squared, adj_r_squared))
+
+    mse_model = calculate_mse_model(sse, dfm)
+    mse_resid = calculate_mse_resid(actual, predictions)
+
+    f_value = calculate_f_value(mse_model, mse_resid)
+    f_pvalue = calculate_fp_value(f_value, dfm, dfr)
+
+    print("F-statistic: {} on {} and {} DF, p-value: {}".format(round(f_value, 3), dfm, dfr, f_pvalue))
+
+    return residuals
+
+
+# -------------- BEGIN STATISTICS HELPER FUNCTIONS ---------------------
+
+def calculate_ssr(actual, predictions):
+    ''' returns value residual sum of squared errors '''
+    ssr = np.sum((actual.reshape(len(actual), 1) - predictions) ** 2)
+    return ssr
+
+
+def calculate_sse(actual, predictions):
+    ''' returns value sum of squared errors '''
+    ybar = np.mean(actual)
+    sse = np.sum((predictions-ybar)**2)
+    return sse
+
+
+def calculate_sst(actual, predictions):
+    ''' returns value total sum of squared errors '''
+    ybar = np.mean(actual)
+    sst = np.sum((actual - ybar)**2) 
+    return sst
+
+
+def AIC(n, sse, ssr, dfm, actual, predictions):
+    ''' returns value for Akaike's information criteria '''
+    ll = loglike(n, sse, ssr, dfm, actual, predictions)
+    aic = 2*(dfm+1)-2*ll
+    return aic
+
+
+def loglike(n, sse, ssr, dfm, actual, predictions):
+    ''' returns value for the log likelihood '''
+    rse = sse / (n-dfm)
+
+    nobs2 = n / 2.0
+    llf = -math.log(ssr) * nobs2            # concentrated likelihood
+    llf -= (1 + math.log(np.pi/nobs2))*nobs2  # with likelihood constant
+    llf -= 1/(2*math.log(rse))
+
+    return round(llf, 0)
+
+
+def extractAIC(n, p, actual, predictions):
+    ''' returns value for the extracted Akaike's information criteria '''
+    sse = calculate_sse(actual, predictions)
+    eaic = n*math.log(sse/n)+2*p
+    return eaic
+
+
+def calculate_r_squared(ssr, sst):
+    ''' returns calculated value of r^2 '''
+    r = 1 - (ssr/sst)
+    return r
+
+
+def calculate_adj_r_squared(n, ssr, sst, dfr, dfm):
+    ''' returns calculated value of adjusted r^2 '''
+    adjr = 1 - np.divide(n - 1, dfr) * (1 - calculate_r_squared(ssr, sst))
+    return adjr
+
+
+def calculate_mse_model(sse, dfm):
+    ''' returns calculated value of the model mean squared error '''
+    return sse/dfm
+
+def calculate_mse_total(sst, dfr, dfm):
+    ''' returns calculated value of the total mean squared error '''
+    return sst / (dfr + dfm)
+
+
+def calculate_mse_resid(actual, predictions):
+    ''' returns calculated value of the residual mean squared error '''
+    return mean_squared_error(actual, predictions)
+
+
+def calculate_f_value(mse_model, dfr):
+    ''' returns calculated value of the model's F statistic '''
+    return mse_model / (dfr * 1.0)
+
+
+def calculate_fp_value(fvalue, dfm, dfr):
+    ''' returns calculated value of the model's p statistic '''
+    fp = stats.f.sf(fvalue, dfm, dfr)
+    fp = "%#6.3g" % fp
+    return fp
+
+
+# -------------- END STATISTICS HELPER FUNCTIONS ---------------------
 
 if __name__ == "__main__":
     main()
